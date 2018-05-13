c97845b4709326e60cfb8af74d48850f0f4163b5
nil2 <nil2@nil2.org>
2018-05-13 11:57:55 +0900

    Execute 'minil new App::rant'

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::rant - It's new $module
+
+# SYNOPSIS
+
+    use App::rant;
+
+# DESCRIPTION
+
+App::rant is ...
+
+# LICENSE
+
+Copyright (C) nil2.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nil2 <nil2@nil2.org>
---
+++ lib/App/rant.pm
@@ -0,0 +1,39 @@
+package App::rant;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::rant - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::rant;
+
+=head1 DESCRIPTION
+
+App::rant is ...
+
+=head1 LICENSE
+
+Copyright (C) nil2.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nil2 E<lt>nil2@nil2.orgE<gt>
+
+=cut
+
