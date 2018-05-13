85b458c18786d2b9f1c4a1396e8639c57db749c9
nil2 <nil2@nil2.org>
2018-05-13 11:59:03 +0900

    Write POD of App::rant

--- lib/App/rant.pm
+++ lib/App/rant.pm
@@ -5,8 +5,6 @@ use warnings;
 
 our $VERSION = "0.01";
 
-
-
 1;
 __END__
 
@@ -14,15 +12,120 @@ __END__
 
 =head1 NAME
 
-App::rant - It's new $module
+App::rant - generate a random text from the regex
 
 =head1 SYNOPSIS
 
-    use App::rant;
+    rant [OPTIONS]...
+
+    rant
+    > rEArCIba
+
+    rant -e'\(/-_-\)/ < [x_]{20}'
+    > (/-_-)/ < ____xxxx__x_xx_xxx_x
 
 =head1 DESCRIPTION
 
-App::rant is ...
+App::rant is a script to generate a random text from the regex.
+
+=head1 INSTALLATION
+
+=head2 With cpanm
+
+    cpanm git://github.com/kusabashira/App-rant
+
+=head2 From source
+
+    git clone git://github.com/kusabashira/App-rant
+    cd App-rant
+    perl Build.PL
+    ./Build
+    ./Build install
+
+=head1 OPTIONS
+
+=head2 -e, --expression=regex
+
+The regex to generate a text (default: `\w{8}`)
+
+Following grammers are supported:
+
+    \w    Alphanumeric + "_"
+    \d    Digits
+    \W    Printable characters other than those in \w
+    \D    Printable characters other than those in \d
+    .     Printable characters
+    []    Character classes
+    {}    Repetition
+    *     Same as {0,}
+    ?     Same as {0,1}
+    +     Same as {1,}
+
+=head3 EXAMPLE
+
+    rant -e'200'
+    > 200
+
+    rant -e'[1-9]00'
+    > 600
+
+    rant -e'[1-9][0-9]{10}'
+    > 33572584712
+
+=head2 -c, --count=n
+
+Print random texts n times (default: `1`)
+
+=head3 EXAMPLE
+
+    rant -c4
+    > gK1Mdux_
+    > YE5ATK15
+    > gFv83LLD
+    > s_WCT7aj
+
+    rant -e'[1-9]00' -c3
+    > 900
+    > 200
+    > 700
+
+    rant -e'[a-z]' -c9
+    > L
+    > S
+    > O
+    > 4
+    > q
+    > J
+    > n
+    > p
+    > N
+
+=head2 -s, --separator=sep
+
+Print sep between each text (default: `\n`)
+
+=head3 EXAMPLE
+
+    rant -c4 -s--
+    > wUBoPdF3--zGUMASow--yHeTYKDk--ytXSjh_u
+
+    rant -e'[ox]' -c20 -s,
+    > x,x,x,o,x,x,x,o,x,x,x,x,x,x,o,x,x,o,x,o
+
+    rant -e'"\w{10}"' -c5 -s $'\n+ '
+    > "S5ofVHoA65"
+    > + "AsOg6jluU9"
+    > + "KcnmEInDpJ"
+    > + "NM49Umwh6r"
+    > + "9dG1n5tMvG"
+
+=head2 --help
+
+Display usage and exit.
+
+=head2 --version
+
+Display version information and exit.
 
 =head1 LICENSE
 
